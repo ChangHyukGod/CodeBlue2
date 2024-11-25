0406537c346e6db63d222f3e19fa4b52168ff021
ChangHyukGod <117065614+ChangHyukGod@users.noreply.github.com>
2024-11-25 09:53:32 +0900

    뼈대
    뼈대

---
+++ Codeblue/front/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 머리말 : HeaderCom -->
+  <HeaderCom />
+  <!-- 본문 -->
+  <div class="container mt-3">
+    <router-view />
+  </div>
+</template>
+<script>
+import HeaderCom from "./components/HeaderCom.vue";
+export default {
+  components: {
+    HeaderCom,
+  },
+};
+</script>
+<style>
+@import "@/assets/css/main.css";
+</style>
