fc59117369c72b51e40ec7f169c18e5a10b933b4
Snuckle110 <103734337+Snuckle110@users.noreply.github.com>
2024-11-26 14:08:26 +0900

    11.26 FAQ, HEADER, FOOTER
    FHF

--- Codeblue/front/src/router/index.js
+++ Codeblue/front/src/router/index.js
@@ -1,16 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    component: HomeView
-  }
-]
+    path: "/",
+    component: HomeView,
+  },
+  {
+    path: "/faq",
+    component: () => import("../views/FaqMain.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
